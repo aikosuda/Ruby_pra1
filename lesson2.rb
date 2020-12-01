24fff055fb413b20f981e7baeab4a2b65c6035f8
須田藍子 <aiko05.hello@gmail.com>
2020-12-01 19:39:48 +0900

    lesson2まで終了

---
+++ lesson2.rb
@@ -0,0 +1,11 @@
+puts 5
+puts "5"
+puts 5 + 3
+puts "5 + 3"
+puts "5" + "3"
+puts "I" + "am" + "Sam"
+
+puts "Samの年齢は" + 27.to_s + "です"
+puts 100 + "200".to_i
+
+puts "私の名前は須田藍子です。年齢は" + 26.to_s + "歳です。"
